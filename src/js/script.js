c38b3294969c3a216dac5b8a21e5a7c367a41484
Alexander <alexmal74@gmail.com>
2021-10-04 16:13:55 +0500

    slick-slider was made, carousel section

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+  });
